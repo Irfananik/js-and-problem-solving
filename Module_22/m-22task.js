d6f400a22f5844049860baf26acfa97c15f33ef3
Golam Morshed Bhuiyan <69519988+Irfananik@users.noreply.github.com>
2022-02-02 09:45:46 +0600

    another problem addad

--- Module_22/m-22task.js
+++ Module_22/m-22task.js
@@ -34,9 +34,46 @@ console.log(reversLetter); */
 
 
 // Task - 03  sum two numbers with function........................................
-function sumTwoNumbers(a, b) {
+/* function sumTwoNumbers(a, b) {
     const sum = a + b;
     return sum;
 }
 let result = sumTwoNumbers(12, 11);
-console.log(result);
+console.log(result); */
+
+
+// Task - 04 Use add and multiplication to calculate wood requirements...........................................
+/* function furnitureCalculation(computerTableQuantity, computerChairQuantity, badQuantity, diningTableQuantity) {
+    const tablePerCount = 10;
+    const chairPerCount = 3;
+    const diningTablePerCount = 20;
+    const bedPerCount = 50;
+
+
+    const computerTable = computerTableQuantity * tablePerCount;
+    const computerChair = computerChairQuantity * chairPerCount;
+    const bedLarge = badQuantity * bedPerCount;
+    const diningTable = diningTableQuantity * diningTablePerCount;
+
+    const totalCount = computerTable + computerChair + bedLarge + diningTable;
+
+    return totalCount;
+}
+
+const firstLot = furnitureCalculation(1, 1, 1, 1);
+console.log(firstLot); */
+
+
+
+// Task - 5 1 to 50 print then Write foo, bar, foobar if divisible by 3 or 5 or both..............................................
+for (let i = 1; i <= 30; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+        console.log("fobar");
+    } else if (i % 3 == 0) {
+        console.log("foo");
+    } else if (i % 5 == 0) {
+        console.log("bar");
+    } else {
+        console.log(i);
+    }
+}
