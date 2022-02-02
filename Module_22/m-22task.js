d9f080491b4d26ed3fab39a993f243005382bfb4
Golam Morshed Bhuiyan <69519988+Irfananik@users.noreply.github.com>
2022-02-02 13:17:26 +0600

    another problem addad

--- Module_22/m-22task.js
+++ Module_22/m-22task.js
@@ -66,7 +66,7 @@ console.log(firstLot); */
 
 
 // Task - 5 1 to 50 print then Write foo, bar, foobar if divisible by 3 or 5 or both..............................................
-for (let i = 1; i <= 30; i++) {
+/* for (let i = 1; i <= 30; i++) {
     if (i % 3 == 0 || i % 5 == 0) {
         console.log("fobar");
     } else if (i % 3 == 0) {
@@ -76,4 +76,53 @@ for (let i = 1; i <= 30; i++) {
     } else {
         console.log(i);
     }
+} */
+
+
+
+// Task - 06 Find the cheapest phone from an array of phone objects...........................................................
+/* const phones = [
+    { name: "nokia", camera: 8, stored: 32, price: 30000 },
+    { name: "samsung", camera: 8, stored: 32, price: 18000 },
+    { name: "oppo", camera: 8, stored: 32, price: 12000 },
+    { name: "samsung", camera: 8, stored: 32, price: 11000 },
+    { name: "nokia", camera: 8, stored: 32, price: 9000 },
+    { name: "apple", camera: 8, stored: 32, price: 110000 },
+    { name: "nokia", camera: 8, stored: 32, price: 10000 }
+]
+let cheapest = phones[0];
+for (const phone of phones) {
+    if (phone.price < cheapest.price) {
+        cheapest = phone;
+    }
 }
+console.log("Lowest price phone is", cheapest); */
+
+
+// Task 07 Calculate the total cost of the products in a shopping cart.................................................................
+/* const products = [
+    { name: "mobile", price: 46000 },
+    { name: "laptop", price: 76000 },
+    { name: "watch", price: 6000 },
+    { name: "bag", price: 5000 }
+]
+let totalPrice = 0;
+for (let product of products) {
+    totalPrice += product.price
+}
+console.log(totalPrice); */
+
+
+
+const cart = [
+    { name: "mobile", price: 46000, quantity: 1 },
+    { name: "laptop", price: 76000, quantity: 1 },
+    { name: "watch", price: 6000, quantity: 2 },
+    { name: "bag", price: 5000, quantity: 2 }
+]
+let totalPrice = 0;
+for (let product of cart) {
+    productTotal = product.price * product.quantity;
+    totalPrice += productTotal;
+}
+console.log(totalPrice);
