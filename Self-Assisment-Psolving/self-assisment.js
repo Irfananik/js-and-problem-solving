b25178f91b868dd4183fc4e88f0ef23baf16f78c
Golam Morshed Bhuiyan <69519988+Irfananik@users.noreply.github.com>
2022-02-03 14:19:20 +0600

    self assisment task added

---
+++ Self-Assisment-Psolving/self-assisment.js
@@ -0,0 +1,15 @@
+// Task 01 feetToInch convertion...........................................
+function feetToInch(feet) {
+    let feetCalculated = 12 * feet;
+    return feetCalculated;
+}
+const myInch = (feetToInch(10));
+console.log(myInch);
+
+// Task - 02 centimetersToMeter convertion........................................
+function centimeterToMeter(centimeter) {
+    let centimeterCalculated = 0.01 * centimeter;
+    return centimeterCalculated;
+}
+const myCentimeter = (centimeterToMeter(100));
+console.log(myCentimeter);
