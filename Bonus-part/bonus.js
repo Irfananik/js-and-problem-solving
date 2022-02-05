62275cee3a48262ab31f8579fb28b8f2b1f39222
Golam Morshed Bhuiyan <69519988+Irfananik@users.noreply.github.com>
2022-02-06 01:00:02 +0600

    last problem addad

--- Bonus-part/bonus.js
+++ Bonus-part/bonus.js
@@ -17,10 +17,70 @@ randomINumber(1); */
 
 // Understand recursion using sum of numbers
 function recursionSum(i) {
-    console.log(i);
+    // console.log(i);
     if (i == 1) {
         return 1;
     }
     return i + recursionSum(--i);
 }
 console.log(recursionSum(5));
+
+
+
+
+
+// Explore Factorial Recursion using a for loop concept
+function factorial(n) {
+    if (n == 1) {
+        return 1;
+    }
+    return n * factorial(--n);
+}
+console.log(factorial(4));
+
+
+
+
+
+// (advanced) Find the matching product by searching products
+/* const products = [
+    { name: "Mac Laptop", price: 1200000 },
+    { name: "Microsoft Laptop", price: 90000 },
+    { name: "Hp Laptop", price: 65000 },
+    { name: "Iphone 14", price: 130000 },
+    { name: "Samsung phone s 14", price: 95000 },
+    { name: "Apple Watch", price: 21000 },
+    { name: "Samsung Watch", price: 9000 }
+];
+
+function searchProducts(products, searchText) {
+    const searchResult = [];
+    for (let product of products) {
+        if (product.name.includes(searchText)) {
+            searchResult.push(product);
+        }
+    }
+    return searchResult;
+}
+const matched = searchProducts(products, "Laptop");
+console.log(matched); */
+
+
+
+
+
+const products = [
+    { name: "Mac Laptop", price: 1200000 },
+    { name: "Microsoft Laptop", price: 90000 },
+    { name: "Hp Laptop", price: 65000 },
+    { name: "Iphone 14", price: 130000 },
+    { name: "Samsung phone s 14", price: 95000 },
+    { name: "Apple Watch", price: 21000 },
+    { name: "Samsung Watch", price: 9000 }
+];
+for (let product of products) {
+    if (product.price < 20000) {
+        continue;
+    }
+    console.log(product);
+}
